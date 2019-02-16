8c1d644c9c40e44bddf5c8bee1bde4c83494bb41
Ashley Souvannaraj <asouvannaraj@gmail.com>
2019-02-16 12:55:30 -0500

    finished assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-ReactDOM.render(<div>implement a counter and render it here!</div>, document.getElementById('root'))
+const { Component } = React;
+
+class Counter extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            num: 0
+        }
+    }
+    increaseNum() {
+        this.setState({
+            num: this.state.num +1
+        })
+    }
+
+    render () {
+        return (
+        <div>
+            <h1>{this.state.num}</h1>
+            <button onClick={() => this.increaseNum() }>ADD</button>
+        </div> )
+    }
+}
+
+
+ReactDOM.render(<Counter />, document.getElementById('root'))
